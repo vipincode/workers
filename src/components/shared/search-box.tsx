c13585191a0dfd5879cc7e08c2d44266f5f9fff6
Vipin Singh <vipin@byldd.com>
2024-12-28 20:46:50 +0530

    fix: search box backgroud

--- src/components/shared/search-box.tsx
+++ src/components/shared/search-box.tsx
@@ -17,18 +17,10 @@ const SearchBox = ({ setSearchQuery }: SearchBoxProps) => {
   return (
     <div className="max-w-[800px] mx-auto mt-[-20px] relative z-10">
       <div className="flex gap-2">
-        <label className="flex-1 rounded-lg border border-secondary input-md md:input-lg flex items-center gap-2">
-          {/* <div className="bg-white items-center px-2">
-            <div className="flex items-center">
-              <div className="flex-1">
-                <Landmark size={30} className="text-secondary" />
-              </div>
-              <div className="w-[40px] text-[14px] text-gray-400 font-semibold leading-4">Delhi NCR</div>
-            </div>
-          </div> */}
+        <label className="flex-1 rounded-lg border bg-accent border-secondary input-md md:input-lg flex items-center gap-2">
           <input
             type="text"
-            className="grow outline-none w-full text-sm md-text-base"
+            className="grow outline-none w-full text-sm md:text-base bg-transparent"
             placeholder="Search for services... "
             value={inputValue}
             onChange={handleInputChange}
